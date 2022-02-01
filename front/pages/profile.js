8b04b66a8d3d927773a03eb1126a2437a0f3c16e
chloemk <minsung.chloe.kim@gmail.com>
2022-02-01 13:00:52 +0900

    feat: AppLayout & Head added

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const profile = () => {
-	return <div>프로필</div>;
+	return (
+		<>
+			<Head>
+				<meta charSet='utf-8' />
+				<title>내 프로필 | NextBird</title>
+			</Head>
+			<AppLayout>내 프로필 페이지</AppLayout>
+		</>
+	);
 };
 
 export default profile;
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const signup = () => {
-	return <div>사인업</div>;
+	return (
+		<>
+			<Head>
+				<meta charSet='utf-8' />
+				<title>회원가입 | NextBird</title>
+			</Head>
+			<AppLayout>회원가입 페이지</AppLayout>
+		</>
+	);
 };
 
 export default signup;
