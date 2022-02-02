598bd6b73b78f4509d2850d7d4e56189c28d28c5
chloemk <minsung.chloe.kim@gmail.com>
2022-02-02 11:10:00 +0900

    feat: profile page dummy data added

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+import FollowerList from '../components/FollowerList';
 
 const profile = () => {
+	//! 더미데이터
+	const followerList = [
+		{ nickname: '김김' },
+		{ nickname: '이이' },
+		{ nickname: '박박' },
+	];
+	const followingList = [
+		{ nickname: '리리' },
+		{ nickname: '아아' },
+		{ nickname: '사사' },
+	];
+
 	return (
 		<>
 			<Head>
 				<meta charSet='utf-8' />
 				<title>내 프로필 | NextBird</title>
 			</Head>
-			<AppLayout>내 프로필 페이지</AppLayout>
+			<AppLayout>
+				<NicknameEditForm />
+				<FollowList header='팔로잉 목록' data={followerList} />
+				<FollowerList header='팔로워 목록' data={followingList} />
+			</AppLayout>
 		</>
 	);
 };
